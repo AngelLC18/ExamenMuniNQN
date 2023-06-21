ab0b9e0f28b28fcee2cee388616bd32015d9f0d5
Angel Luciano Ciancio <ciancioangel.epet20@gmail.com>
2023-06-21 13:00:54 -0300

    21/6/2023

--- examen-muni-nqn-frontend/src/routes/routes.js
+++ examen-muni-nqn-frontend/src/routes/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Campus from "../pages/campus/CampusPage.jsx";
+import CreatePersona from "../pages/CreatePersona";
 
 const Rutas = () => {
   return (
@@ -9,6 +10,8 @@ const Rutas = () => {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/campus" element={<Campus />} />
+          <Route path="*" element={<h1>Not Found 404</h1>} />
+          <Route path="/formulario" element={<CreatePersona />} />
         </Routes>
       </BrowserRouter>
     </div>
